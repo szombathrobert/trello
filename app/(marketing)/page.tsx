3e1792cd2f5a1b2cc973352792336b60950fe88c
szombathrobert4 <szombathrobert4@gmail.com>
2024-03-20 11:14:11 +0100

    Marketing Page Done!

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -35,24 +35,24 @@ const MarketingPage = () => {
                 )}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2"/>
-                    No 1 task management
+                    No 1 feladat management
                 </div>
                 <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-                    Taskify helps team move
+                    Taskify segíti a csapatot
                 </h1>
                 <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-                    Work forward.
+                   előre dolgozni.
                 </div>
             </div>
             <div className={cn(
                 "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
                 textFont.className,
                 )}>
-                Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+                Együttműküdés, Project menedzselés, és új produktivitás elérése.A magas épületektől a home officig, a csapat munkamódszere egyedülálló - Érje el mindezt a Taskify segítségével.
             </div>
             <Button className="mt-6" size="lg" asChild>
                 <Link href="/sign-up">
-                    Get Taskify for free
+                    Regisztráció
                 </Link>
             </Button>
         </div>
