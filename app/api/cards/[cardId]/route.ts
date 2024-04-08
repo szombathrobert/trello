57a0cf2d9172f2b96e15a447cd34a9eb8a7432bc
szombathrobert <szombathrobert4@gmail.com>
2024-04-08 09:20:17 +0200

    route fixe

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -3,37 +3,37 @@ import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
 
-export async function GET (
-    req: Request,
-    { params }: { params: { cardId: string } }
+export async function GET(
+  req: Request,
+  { params }: { params: { cardId: string } }
 ) {
-    try {
-        const { userId, orgId } = auth();
+  try {
+    const { userId, orgId } = auth();
 
-        if (!userId || !orgId) {
-            return new NextResponse("Azonosítatlan!", { status: 401 });
-        }
-
-        const card = await db.card.findUnique({
-            where: {
-                id: params.cardId,
-                list: {
-                    board: {
-                        orgId,
-                    },
-                },
-            },
-            include: {
-                list: {
-                    select: {
-                        title: true,
-                    },
-                },
-            },
-        });
-
-        return NextResponse.json(card);
-    } catch (error) {
-        return new NextResponse("Internal Error", { status: 500 });
+    if (!userId || !orgId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const card = await db.card.findUnique({
+      where: {
+        id: params.cardId,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+      include: {
+        list: {
+          select: {
+            title: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(card);
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
